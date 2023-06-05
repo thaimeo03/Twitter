cdd0f7beafa969aee409e70441b6e219b8bcc506
Thai <thaitran15072003@gmail.com>
2023-06-05 22:49:46 +0700

    Create User schema

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express'
+import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
-  res.json({ message: 'Login successfully' })
+  return res.json({ message: 'Login successfully' })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const result = await usersService.register({ email, password })
+    return res.json({ message: 'Register successfully', result })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ message: 'Register failed' })
+  }
 }
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,25 +1,32 @@
-import { MongoClient } from 'mongodb'
+import { Collection, Db, MongoClient } from 'mongodb'
 import 'dotenv/config'
+import User from '~/models/schemas/User.schema'
+import { error } from 'console'
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.3menitg.mongodb.net/?retryWrites=true&w=majority`
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
+
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
 
   async connect() {
     try {
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
     } catch (err) {
       console.log(err)
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
+      throw error
     }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
   }
 }
 
