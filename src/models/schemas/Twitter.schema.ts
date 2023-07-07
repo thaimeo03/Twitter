7ba66b9fdd9fe4dbf9ffbaeb64cc72d23e085cd2
Thai <thaitran15072003@gmail.com>
2023-07-07 10:11:15 +0700

    declare Tweet schema

---
+++ src/models/schemas/Twitter.schema.ts
@@ -0,0 +1,52 @@
+import { ObjectId } from 'mongodb'
+import { TweetAudience, TweetType } from '~/constants/enums'
+import { Media } from '../interfaces/media.interfaces'
+
+interface TweetConstructorType {
+  _id?: ObjectId
+  user_id: ObjectId
+  type: TweetType
+  audience: TweetAudience
+  content: string
+  parent_id: null | ObjectId //  chỉ null khi tweet gốc
+  hashtags: ObjectId[]
+  mentions: ObjectId[]
+  medias: Media[]
+  guest_views: number
+  user_views: number
+  created_at?: Date
+  updated_at?: Date
+}
+
+export default class Tweet {
+  _id: ObjectId
+  user_id: ObjectId
+  type: TweetType
+  audience: TweetAudience
+  content: string
+  parent_id: null | ObjectId
+  hashtags: ObjectId[]
+  mentions: ObjectId[]
+  medias: Media[]
+  guest_views: number
+  user_views: number
+  created_at: Date
+  updated_at: Date
+
+  constructor(tweet: TweetConstructorType) {
+    const date = new Date()
+    this._id = tweet._id || new ObjectId()
+    this.user_id = tweet.user_id
+    this.type = tweet.type
+    this.audience = tweet.audience
+    this.content = tweet.content
+    this.parent_id = tweet.parent_id
+    this.hashtags = tweet.hashtags
+    this.mentions = tweet.mentions
+    this.medias = tweet.medias
+    this.guest_views = tweet.guest_views
+    this.user_views = tweet.user_views
+    this.created_at = tweet.created_at || date
+    this.updated_at = tweet.updated_at || date
+  }
+}
