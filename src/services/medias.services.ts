ab6745cbd506e8f3fad9080ea94c5c88257008dc
Thai <thaitran15072003@gmail.com>
2023-07-27 09:22:57 +0700

    upload image and video file to s3

--- src/services/medias.services.ts
+++ src/services/medias.services.ts
@@ -3,12 +3,15 @@ import path from 'path'
 import sharp from 'sharp'
 import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
 import { getNameImage, uploadImageFile, uploadVideoFile } from '~/utils/file'
-import fs from 'fs'
-import 'dotenv/config'
+import fsPromise from 'fs/promises'
 import { isProduction } from '~/constants/congfig'
 import { MediaType } from '~/constants/enums'
 import { Media } from '~/models/interfaces/media.interfaces'
 import { encodeHLSWithMultipleVideoStreams } from '~/utils/video'
+import { uploadImageToS3, uploadVideoToS3 } from '~/utils/s3'
+import mime from 'mime'
+import 'dotenv/config'
+import { CompleteMultipartUploadCommandOutput } from '@aws-sdk/client-s3'
 
 class MediasService {
   async uploadImage(req: Request) {
@@ -20,12 +23,17 @@ class MediasService {
         const pathImage = path.join(UPLOAD_IMAGE_DIR, `${nameImage}.jpg`)
 
         await sharp(imageData.filepath).jpeg().toFile(pathImage)
-        fs.unlinkSync(imageData.filepath)
+
+        const res = await uploadImageToS3({
+          nameFile: `${nameImage}.jpg`,
+          filepath: pathImage,
+          type: mime.getType(pathImage) as string
+        })
+
+        await Promise.all([fsPromise.unlink(imageData.filepath), fsPromise.unlink(pathImage)])
 
         return {
-          url: isProduction
-            ? `${process.env.HOST}/static/image/${imageData.newFilename}`
-            : `http://localhost:${process.env.PORT}/static/image/${imageData.newFilename}`,
+          url: (res as CompleteMultipartUploadCommandOutput).Location as string,
           type: MediaType.Image
         }
       })
@@ -39,10 +47,14 @@ class MediasService {
 
     const result: Media[] = await Promise.all(
       videosData.map(async (video) => {
+        const res = await uploadVideoToS3({
+          nameFile: video.newFilename,
+          filepath: video.filepath,
+          type: mime.getType(video.filepath) as string
+        })
+
         return {
-          url: isProduction
-            ? `${process.env.HOST}/medias/video-stream/${video.newFilename}`
-            : `http://localhost:${process.env.PORT}/medias/video-stream/${video.newFilename}`,
+          url: (res as CompleteMultipartUploadCommandOutput).Location as string,
           type: MediaType.Video
         }
       })
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,5 +1,7 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
 import 'dotenv/config'
+import fs from 'fs'
 
 const s3 = new S3({
   region: process.env.AWS_REGION,
@@ -8,4 +10,45 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
-s3.listBuckets({}).then((data) => console.log(data))
+
+export const uploadImageToS3 = ({ nameFile, filepath, type }: { nameFile: string; filepath: string; type: string }) => {
+  const parallelUploads3 = new Upload({
+    client: s3,
+    params: {
+      Bucket: process.env.AWS_BUCKET_NAME as string,
+      Key: `images/${nameFile}`,
+      Body: fs.readFileSync(filepath),
+      ContentType: type
+    },
+
+    tags: [
+      /*...*/
+    ], // optional tags
+    queueSize: 4, // optional concurrency configuration
+    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+    leavePartsOnError: false // optional manually handle dropped parts
+  })
+
+  return parallelUploads3.done()
+}
+
+export const uploadVideoToS3 = ({ nameFile, filepath, type }: { nameFile: string; filepath: string; type: string }) => {
+  const parallelUploads3 = new Upload({
+    client: s3,
+    params: {
+      Bucket: process.env.AWS_BUCKET_NAME as string,
+      Key: `videos/${nameFile}`,
+      Body: fs.readFileSync(filepath),
+      ContentType: type
+    },
+
+    tags: [
+      /*...*/
+    ], // optional tags
+    queueSize: 4, // optional concurrency configuration
+    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+    leavePartsOnError: false // optional manually handle dropped parts
+  })
+
+  return parallelUploads3.done()
+}
